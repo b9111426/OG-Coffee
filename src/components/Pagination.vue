05a302523f1b7fe98c5c335090775a18389edd4c
gary.shine <b9111426@gmail.com>
2022-10-17 22:45:06 +0800

    修改分頁按鈕樣式

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,33 +1,44 @@
 <template>
   <nav aria-label="Page navigation example">
-    <ul class="pagination justify-content-center">
-      <li class="page-item" :class="{ disabled: !pages.has_pre }">
-        <a
-          class="page-link"
-          href="#"
+    <ul class="pagination justify-content-center align-items-center">
+      <li>
+        <button
+          type="button"
+          class="btn text-primary bg-transparent border-0"
+          :disabled="!pages.has_pre"
           @click.prevent="updatePage(pages.current_page - 1)"
-          aria-label="Previous"
-          ><span aria-hidden="true">&laquo;</span>
-        </a>
+        >
+          <i
+            class="bi bi-chevron-double-left fs-5"
+            :class="{ 'text-white': !pages.has_pre }"
+          ></i>
+        </button>
       </li>
       <li
-        class="page-item"
+        class="page-item mx-1"
         :class="{ active: page === pages.current_page }"
         v-for="page in pages.total_pages"
         :key="page + '452'"
       >
-        <a class="page-link" href="#" @click.prevent="updatePage(page)">{{
-          page
-        }}</a>
-      </li>
-      <li class="page-item" :class="{ disabled: !pages.has_next }">
         <a
-          class="page-link"
+          class="page-link rounded-circle"
           href="#"
+          @click.prevent="updatePage(page)"
+          >{{ page }}</a
+        >
+      </li>
+      <li>
+        <button
+          type="button"
+          class="btn text-primary bg-transparent border-0"
+          :disabled="!pages.has_next"
           @click.prevent="updatePage(pages.current_page + 1)"
-          aria-label="Next"
-          ><span aria-hidden="true">&raquo;</span>
-        </a>
+        >
+          <i
+            class="bi bi-chevron-double-right fs-5"
+            :class="{ 'text-white': !pages.has_next }"
+          ></i>
+        </button>
       </li>
     </ul>
   </nav>
